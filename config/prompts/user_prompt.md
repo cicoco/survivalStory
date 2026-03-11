36c3d51f01161be352ada992ea6434469e457e0d
tafiagu <tafiagu@gmail.com>
2026-03-11 12:34:37 +0800

    feature: 重新设计

--- config/prompts/user_prompt.md
+++
@@ -1,7 +0,0 @@
-当前坐标:({{current_x}},{{current_y}})
-当前时段:{{phase_cn}}
-当前状态:水分-{{water}}，饱食-{{food}}，曝光-{{exposure}}
-背包物品:{{bag_text}}
-当前建筑剩余物资:{{loot_text}}
-当前建筑内其他玩家:{{other_players}}
-请基于以上当前状态，结合系统规则输出一个动作。
--- main.py
+++
@@ -1,5 +0,0 @@
-if __name__ == "__main__":
-    print("请使用 Broker + 统一客户端模式：")
-    print("请使用:")
-    print("  uv run survival-broker --bind 0.0.0.0 --port 9010")
-    print("  uv run survival --broker-host <broker_ip> --broker-port 9010 --name 玩家A")
